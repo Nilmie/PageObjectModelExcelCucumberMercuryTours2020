19ef2f1ffbe30245b6b02617135e8b4063f401e6
Nilmie Gamhewa <nilmie@mihcm.com>
2020-12-30 14:37:37 +0530

    initial commit

---
+++ src/main/java/Features/Login.feature
@@ -0,0 +1,10 @@
+Feature: Guru 99 login
+  Scenario: Register a user in Mercury Tours!
+
+    Given I have user Details
+ 
+    
+    When I successfully submit  
+ 
+
+    Then I should successfully registered
---
+++ src/main/java/StepDefinition/Register.java
@@ -0,0 +1,66 @@
+package StepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import MiHCM.HomePage;
+import MiHCM.LoginPage;
+import Utility.ReadExcel;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Register {
+	
+	WebDriver driver;
+
+    LoginPage objLogin;
+    HomePage objHomePage;
+    ReadExcel excel = new ReadExcel();
+	
+
+	@Given("^I have user Details$")
+	public void i_have_user_Details() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver","C:\\seldrv\\chromedriver.exe");
+		
+	    driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://demo.guru99.com/test/newtours/");
+        //driver.findElement(By.xpath("(//form[@name='home' ]//table)[5]")).click();
+        driver.findElement(By.linkText("Register here")).click();
+	    
+	}
+	
+	@When("^I successfully submit$")
+	public void i_successfully_submit() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		objLogin = new LoginPage(driver);
+		
+	//	String loginPageTitle = objLogin.getLoginTitle();
+	    objLogin.RegisterUser(excel.readExcel(1,1, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,2, ".\\Data\\data.xlsx","Sheet1"),excel.readExcel(1,3, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,4, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,5, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,6, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,7, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,8, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,10, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,11, ".\\Data\\data.xlsx","Sheet1"), excel.readExcel(1,12, ".\\Data\\data.xlsx","Sheet1"));
+	    //excel.readExcel(1,9, ".\\Data\\data.xlsx","Sheet1"),
+	
+	}
+	
+	@Then("^I should successfully registered$")
+	public void i_should_successfully_registered() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		objHomePage = new HomePage(driver);
+
+		   //Verify home page
+
+		    Assert.assertTrue(objHomePage.getHomePageDashboardUserName().contains("sign-in"));
+		    driver.close();
+	    
+}
+
+
+}
